67343f76dfe0c5e5b14848bb809afcf70cad6bd5
Avinash <avinashkrts@gmail.com>
2022-02-26 20:39:19 +0530

    all

---
+++ src/screens/customer/product/offerdata.ts
@@ -0,0 +1,27 @@
+import { AppConstants } from "../../../constants"
+
+export const OfferData = [
+    {
+        id: "1",
+        image: "1_" + AppConstants.SHOP_ID + "_offer.png",
+        name: 'HOT_SELL',
+        label: "Hot Sell"
+    },
+    {
+        id: '2',
+        image: "2_" + AppConstants.SHOP_ID + "_offer.png",
+        name: 'BAGGAGE',
+        label: "Baggage"
+    },
+    {
+        id: '3',
+        image: "3_" + AppConstants.SHOP_ID + "_offer.png",
+        name: 'MILAAN_OFFER',
+        label: "Milaan Offer"
+    },
+    {
+        id: '4',
+        image: "4_" + AppConstants.SHOP_ID + "_offer.png",
+        name: 'ADITIONAL_DISCOUNT',
+        label: "Aditional Discount"
+    }]
